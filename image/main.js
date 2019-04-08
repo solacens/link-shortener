7104373348194bcfa79f12f14a624903c537df6b
solacens <yippw1025@gmail.com>
2019-04-08 19:07:49 +0800

    rapid in memory prototype

--- image/main.js
+++ image/main.js
@@ -3,20 +3,56 @@
 const PORT = 8080
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/ping', (req, res) => {
+const randomIdGenerator = () => {
+  var text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 8; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text;
+}
+
+const inMemoryDb = {}
+
+const addRecord = (url) => {
+  var randomId = randomIdGenerator()
+  while (typeof inMemoryDb[randomId] !== 'undefined'){
+    randomId = randomIdGenerator()
+  }
+  inMemoryDb[randomId] = { url }
+  return randomId
+}
+
+const getRecord = (shortLink) => {
+  return typeof inMemoryDb[shortLink] === 'undefined' ? null : inMemoryDb[shortLink]
+}
+
+app.use(bodyParser.json())
+
+app.get('/ping', (req, res, next) => {
   res.send('pong')
 })
 
-app.post('/submit', (req, res) => {
-  res.send('OK')
+app.post('/submit', (req, res, next) => {
+  const requestObj = req.body
+  const responseObj = {
+    url: requestObj.url,
+    shorten_url: `http://localhost:8080/${addRecord(requestObj.url)}`
+  }
+  res.send(responseObj)
 })
 
-app.get("/:lnk([a-zA-Z0-9]{8})", (req, res) => {
+app.get("/:lnk([a-zA-Z0-9]{8})", (req, res, next) => {
   const shortLink = req.params.lnk
-  res.send('OK')
+  const urlObject = getRecord(shortLink)
+  console.log(urlObject)
+  if (urlObject) {
+    res.redirect(301, urlObject.url)
+  } else {
+    next()
+  }
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`))
